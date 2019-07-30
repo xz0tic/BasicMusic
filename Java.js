49c13f550a3466b0799f9e301caf631bc4f96feb
xz0tic <53493353+xz0tic@users.noreply.github.com>
2019-07-30 15:46:50 -0400

    Create Java.js

---
+++ Java.js
@@ -0,0 +1,20 @@
+function bigImg(x){
+  x.style.height = "405px";
+  x.style.width = "1000px";
+}
+
+function smallImg(x){
+  x.style.height = "205px";
+  x.style.width = "725px";
+}
+
+function bigImg2(y){
+  y.style.width = "1000px";
+  y.style.height = "300px";
+}
+
+function smallImg2(y){
+  y.style.width = "725px";
+  y.style.height = "193px";
+
+}
